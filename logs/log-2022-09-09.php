35e309eec0889c3ff8ea777eea832f581da15763
Desa Garut <desagarut.net@gmail.com>
2022-09-27 17:28:05 +0700

    Delete log-2022-09-09.php

--- logs/log-2022-09-09.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2022-09-09 08:51:03 --> 404 Page Not Found: Film/2022
-ERROR - 2022-09-09 09:04:42 --> 404 Page Not Found: Film/2022
-ERROR - 2022-09-09 09:21:35 --> 404 Page Not Found: Img/team-1.jpg
-ERROR - 2022-09-09 09:21:57 --> 404 Page Not Found: Img/team-1.jpg
-ERROR - 2022-09-09 09:23:37 --> 404 Page Not Found: Img/team-1.jpg
-ERROR - 2022-09-09 09:23:52 --> 404 Page Not Found: Img/team-1.jpg
