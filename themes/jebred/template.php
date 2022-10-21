289cf3f952f051622aafff918a8a2e4910e0bf4e
Desa Garut <desagarut.net@gmail.com>
2022-10-21 15:14:19 +0700

    Update template.php

--- themes/jebred/template.php
+++ themes/jebred/template.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<?php $this->load->view($folder_themes .'/layouts/home.tpl.php') ?>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1823410826720847" crossorigin="anonymous"></script>
+<?php $this->load->view($folder_themes . '/layouts/home.tpl.php') ?>
 </body>
-
 </html>
