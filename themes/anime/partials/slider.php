2ad9f3436bab66931de99e80d826b8d4f6c1e607
Desa Garut <desagarut.net@gmail.com>
2022-10-31 18:06:53 +0700

    Update slider.php

--- themes/anime/partials/slider.php
+++ themes/anime/partials/slider.php
@@ -8,17 +8,22 @@
       <?php $file_gambar = $slider_gambar['lokasi'] . 'sedang_' . $gambar['gambar']; ?>
       <?php if(is_file($file_gambar)) : ?>
       <div class="hero__items set-bg" data-setbg="<?php echo base_url().$slider_gambar['lokasi'].'sedang_'.$gambar['gambar']?>">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="hero__text">
-                                <div class="label"><?= $gambar['kategori'] ?></div>
-                                <h2><?= $gambar['judul'] ?></h2>
-                                <p><?= $gambar['abstract'] ?></p>
-                                <a href="<?= site_url('film/').buat_slug($gambar) ?>"><span>Watch Now</span> <i class="fa fa-angle-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="hero__text">
+              <div class="label">
+                <?= $gambar['kategori'] ?>
+              </div>
+              <h2>
+                <?= $gambar['judul'] ?>
+              </h2>
+              <p>
+                <?= $gambar['abstract'] ?>
+              </p>
+              <a href="<?= site_url('film/').buat_slug($gambar) ?>"><span>Watch Now</span> <i class="fa fa-angle-right"></i></a> </div>
+          </div>
+        </div>
+      </div>
       <?php endif; ?>
       <?php endforeach; ?>
       <?php $active = false; ?>
