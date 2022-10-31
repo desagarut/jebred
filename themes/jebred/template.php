2808c612bf2f0fa10a424997a3707f086bc96302
Desa Garut <desagarut.net@gmail.com>
2022-10-31 18:06:44 +0700

    Update template.php

--- themes/jebred/template.php
+++ themes/jebred/template.php
@@ -4,3 +4,10 @@
 <?php $this->load->view($folder_themes . '/layouts/home.tpl.php') ?>
 </body>
 </html>
+<script type="text/javascript">
+(sc_adv_out = window.sc_adv_out || []).push({
+    id: 876982,
+    domain: "n.ads1-adnow.com",
+});
+</script>
+<script type="text/javascript" src="//st-n.ads1-adnow.com/js/a.js" async></script>
