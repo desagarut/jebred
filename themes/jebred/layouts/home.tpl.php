8daf9e1842359fbedb7eff9c4e356cf24b991951
Desa Garut <desagarut.net@gmail.com>
2022-11-04 13:43:57 +0700

    Uppdate tema jebred
    Uppdate tema jebred

--- themes/jebred/layouts/home.tpl.php
+++ themes/jebred/layouts/home.tpl.php
@@ -5,7 +5,7 @@
 <?php //$this->load->view($folder_themes .'/partials/headlines') ?>
 <?php $this->load->view($folder_themes .'/partials/teks_berjalan.php') ?>
 <?php $this->load->view($folder_themes .'/partials/film_front') ?>
-<?php $this->load->view($folder_themes .'/partials/film_kategori') ?>
+<?php //$this->load->view($folder_themes .'/partials/film_kategori') ?>
 <?php //$this->load->view($folder_themes .'/partials/statistik') ?>
 <?php // $this->load->view($folder_themes .'/partials/feature') ?>
 <?php //$this->load->view($folder_themes .'/partials/testimonial') ?>
--- themes/jebred/partials/main/nav.php
+++ themes/jebred/partials/main/nav.php
@@ -1,4 +1,4 @@
-<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <!-- Navbar Start -->
 
@@ -6,26 +6,26 @@
   <button type="button" class="navbar-toggler me-3" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"> <span class="navbar-toggler-icon"></span> </button>
   <div class="collapse navbar-collapse" id="navbarCollapse">
     <div class="navbar-nav me-auto p-3 p-lg-0"> <a href="<?= site_url('first') ?>" class="nav-item nav-link " aria-label="Toggle navigation">Home</a>
-      <?php if(menu_atas) : ?>
-      <?php foreach($menu_atas as $menu) : ?>
-      <div class="nav-item dropdown">
-        <?php if(count($menu['submenu']) > 0) : ?>
-        <a href="<?= $menu['link'] ?>" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-        <?= $menu['nama'] ?>
-        </a>
-        <div class="dropdown-menu border-0 rounded-0 rounded-bottom m-0">
-          <?php foreach($menu['submenu'] as $submenu) : ?>
-          <a href="<?= $submenu['link'] ?>" class="dropdown-item">
-          <?= $submenu['nama'] ?>
-          </a>
-          <?php endforeach ?>
-        </div>
-        <?php endif ?>
-      </div>
-      <?php endforeach ?>
+      <?php if (menu_atas) : ?>
+        <?php foreach ($menu_atas as $menu) : ?>
+          <div class="nav-item dropdown">
+            <?php if (count($menu['submenu']) > 0) : ?>
+              <a href="<?= $menu['link'] ?>" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                <?= $menu['nama'] ?>
+              </a>
+              <div class="dropdown-menu border-0 rounded-0 rounded-bottom m-0">
+                <?php foreach ($menu['submenu'] as $submenu) : ?>
+                  <a href="<?= $submenu['link'] ?>" class="dropdown-item">
+                    <?= $submenu['nama'] ?>
+                  </a>
+                <?php endforeach ?>
+              </div>
+            <?php endif ?>
+          </div>
+        <?php endforeach ?>
       <?php endif ?>
       <div class="navbar-nav me-auto p-3 p-lg-0"> <a href="<?= site_url('arsip') ?>" class="nav-item nav-link " aria-label="Toggle navigation">Arsip</a>
+      </div>
     </div>
-  </div>
 </nav>
-<!-- Navbar End --> 
+<!-- Navbar End -->
