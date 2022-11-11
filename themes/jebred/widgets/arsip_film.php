be90b498d10c09cd222808c700bcb87aa0f1f626
Desa Garut <desagarut.net@gmail.com>
2022-11-12 02:01:18 +0700

    Update arsip_film.php

--- themes/jebred/widgets/arsip_film.php
+++ themes/jebred/widgets/arsip_film.php
@@ -40,6 +40,15 @@
 		</div>
 	<?php endforeach ?>
 </div>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1823410826720847"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:inline-block;width:360px;height:800px"
+     data-ad-client="ca-pub-1823410826720847"
+     data-ad-slot="2492499723"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 
 <script>
 	var firstTabEl = document.querySelector('#myTab li:last-child a')
