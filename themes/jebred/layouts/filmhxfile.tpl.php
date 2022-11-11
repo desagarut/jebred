1ff185496e0e043305d5f99b3f2302cd60ff9078
Desa Garut <desagarut.net@gmail.com>
2022-11-11 15:24:14 +0700

    No Load
    No Loading style

--- themes/jebred/layouts/filmhxfile.tpl.php
+++ themes/jebred/layouts/filmhxfile.tpl.php
@@ -9,11 +9,11 @@
 </head>
 
 <body>
-<!-- Spinner Start -->
+<!-- Spinner Start
 <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
   <div class="spinner-border position-relative text-danger" style="width: 6rem; height: 6rem;" role="status"></div>
   <i class="fa fa-laptop-code fa-2x text-danger position-absolute top-50 start-50 translate-middle"></i> </div>
-<!-- Spinner End -->
+Spinner End --> 
 <?php $this->load->view($folder_themes . '/partials/main/header') ?>
 <?php $this->load->view($folder_themes . '/partials/main/nav') ?>
 <?php $this->load->view($folder_themes . '/partials/teks_berjalan.php') ?>
