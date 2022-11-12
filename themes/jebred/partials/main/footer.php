f4f4c5e8e3ca92f7c40a000b22a93e22a2f2e500
Desa Garut <desagarut.net@gmail.com>
2022-11-12 13:25:35 +0700

    Update footer.php

--- themes/jebred/partials/main/footer.php
+++ themes/jebred/partials/main/footer.php
@@ -3,34 +3,27 @@
 <!-- Footer Start -->
 
 <div class="container-fluid bg-dark text-body footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
-<!--  <div class="container py-5">
+  <div class="container py-5">
     <div class="row g-5">
       <div class="col-lg-3 col-md-6">
         <h5 class="text-light mb-4">Address</h5>
-        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>
-          <?= $kabupaten['alamat']; ?>
-        </p>
-        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>
-          <?= $kabupaten['telepon']; ?>
-        </p>
-        <p class="mb-2"><i class="fa fa-envelope me-3"></i>
-          <?= $kabupaten['email']; ?>
+        <p class="mb-2">
+          Jebred Movies adalah sebuah website hiburan yang menyajikan streaming film atau download movie gratis. Subtitle Indonesa.
+          Perlu diketahui, film-film yang terdapat pada web ini didapatkan dari web pencarian di internet. Kami tidak menyimpan file film tersebut di server sendiri dan kami hanya menempelkan link-link tersebut di website kami.
         </p>
         <div class="d-flex pt-2"> <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href=""><i class="fab fa-twitter"></i></a> <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href=""><i class="fab fa-facebook-f"></i></a> <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href=""><i class="fab fa-youtube"></i></a> <a class="btn btn-square btn-outline-secondary rounded-circle me-0" href=""><i class="fab fa-linkedin-in"></i></a> </div>
       </div>
       <div class="col-lg-3 col-md-6">
-        <h5 class="text-light mb-4">Quick Links</h5>
-        <a class="btn btn-link" href="">About Us</a> <a class="btn btn-link" href="">Contact Us</a> <a class="btn btn-link" href="">Our Services</a> <a class="btn btn-link" href="">Terms & Condition</a> <a class="btn btn-link" href="https://help.desagarut.id">Support</a> </div>
+        <h5 class="text-light mb-4">Kategori</h5>
+        <?php foreach($menu_kiri as $data):?>
+        <a class="btn btn-link" href="<?= site_url("film/kategori/$data[slug]"); ?>"><?= $data['kategori']; ?><?php (count($data['submenu'])>0) and print('<span class="caret"></span>');?></a> 
+        <?php endforeach;?>
+      </div>
       <div class="col-lg-3 col-md-6">
-        <h5 class="text-light mb-4">Gallery</h5>
-        <div class="row g-2">
-          <div class="col-4"> <img class="img-fluid rounded" src="<?= base_url("$this->theme_folder/$this->theme/img/project-1.jpg") ?>" alt="Image"> </div>
-          <div class="col-4"> <img class="img-fluid rounded" src="<?= base_url("$this->theme_folder/$this->theme/img/project-2.jpg") ?>" alt="Image"> </div>
-          <div class="col-4"> <img class="img-fluid rounded" src="<?= base_url("$this->theme_folder/$this->theme/img/project-3.jpg") ?>" alt="Image"> </div>
-          <div class="col-4"> <img class="img-fluid rounded" src="<?= base_url("$this->theme_folder/$this->theme/img/project-4.jpg") ?>" alt="Image"> </div>
-          <div class="col-4"> <img class="img-fluid rounded" src="<?= base_url("$this->theme_folder/$this->theme/img/project-5.jpg") ?>" alt="Image"> </div>
-          <div class="col-4"> <img class="img-fluid rounded" src="<?= base_url("$this->theme_folder/$this->theme/img/project-6.jpg") ?>" alt="Image"> </div>
-        </div>
+        <h5 class="text-light mb-4">Genre</h5>
+        <?php foreach($menu_kiri as $data):?>
+        <a class="btn btn-link" href="<?= site_url("film/kategori/$data[slug]"); ?>"><?= $data['kategori']; ?><?php (count($data['submenu'])>0) and print('<span class="caret"></span>');?></a> 
+        <?php endforeach;?>
       </div>
       <div class="col-lg-3 col-md-6">
         <h5 class="text-light mb-4">Newsletter</h5>
@@ -41,32 +34,32 @@
         </div>
       </div>
     </div>
-  </div>-->
+  </div>
   <div class="container-fluid copyright">
     <div class="container">
       <div class="row">
         <div class="col-md-6 text-center text-md-start mb-3 mb-md-0"> &copy; <a href="#">
-          <?= $this->setting->website_title ?>
+            <?= $this->setting->website_title ?>
           </a>, All Right Reserved. </div>
         <div class="col-md-6 text-center text-md-end"> </div>
       </div>
     </div>
   </div>
 </div>
-<!-- Footer End --> 
+<!-- Footer End -->
 
-<!-- Back to Top --> 
-<a href="#" class="btn btn-lg btn-danger btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a> 
+<!-- Back to Top -->
+<a href="#" class="btn btn-lg btn-danger btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
 
-<!-- JavaScript Libraries --> 
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script> 
-<script src="<?= base_url("$this->theme_folder/$this->theme/lib/wow/wow.min.js") ?>"></script> 
-<script src="<?= base_url("$this->theme_folder/$this->theme/lib/easing/easing.min.js") ?>"></script> 
-<script src="<?= base_url("$this->theme_folder/$this->theme/lib/waypoints/waypoints.min.js") ?>"></script> 
-<script src="<?= base_url("$this->theme_folder/$this->theme/lib/counterup/counterup.min.js") ?>"></script> 
-<script src="<?= base_url("$this->theme_folder/$this->theme/lib/owlcarousel/owl.carousel.min.js") ?>"></script> 
-<script src="<?= base_url("$this->theme_folder/$this->theme/lib/lightbox/js/lightbox.min.js") ?>"></script> 
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url("$this->theme_folder/$this->theme/lib/wow/wow.min.js") ?>"></script>
+<script src="<?= base_url("$this->theme_folder/$this->theme/lib/easing/easing.min.js") ?>"></script>
+<script src="<?= base_url("$this->theme_folder/$this->theme/lib/waypoints/waypoints.min.js") ?>"></script>
+<script src="<?= base_url("$this->theme_folder/$this->theme/lib/counterup/counterup.min.js") ?>"></script>
+<script src="<?= base_url("$this->theme_folder/$this->theme/lib/owlcarousel/owl.carousel.min.js") ?>"></script>
+<script src="<?= base_url("$this->theme_folder/$this->theme/lib/lightbox/js/lightbox.min.js") ?>"></script>
 
-<!-- Template Javascript --> 
+<!-- Template Javascript -->
 <script src="<?= base_url("$this->theme_folder/$this->theme/js/main.js") ?>"></script>
