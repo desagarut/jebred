8e7967bcbe5f28d22c157140e2e6e4b6cfa9b47f
Desa Garut <desagarut.net@gmail.com>
2022-11-18 11:03:55 +0700

    Update slider.php

--- themes/jebred/partials/slider.php
+++ themes/jebred/partials/slider.php
@@ -19,7 +19,7 @@
                                         <h1 class="display-1 text-white mb-2 animated zoomIn"><?= $gambar['judul'] ?></h1>
                                         </a>
                                         <h6 class="text-primary text-uppercase mb-2 animated zoomIn"><?= $gambar['genre1'] ?> <?= $gambar['negara1'] ?></h6>
-                                        <p><?= potong_teks($gambar['isi'], 250) ?> </p>
+                                        <p><?= potong_teks($gambar['isi'], 250) ?>... </p>
                                         <a href="#" class="btn btn-lg btn-success">Sinopsis</a>&nbsp;<a href="<?= 'film/' . buat_slug($gambar); ?>" class="btn btn-lg btn-warning">Movie</a>
                                     </div>
                                 </div>
