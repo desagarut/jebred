4f0348031afa9aee6687e58260fd1dbf8ad12ccb
Desa Garut <desagarut.net@gmail.com>
2022-11-24 11:09:12 +0700

    Update slider_artikel_front.php

--- themes/jebred/partials/slider_artikel_front.php
+++ themes/jebred/partials/slider_artikel_front.php
@@ -3,6 +3,11 @@
 <!-- Slider Artikel -->
 <div class="container-xxl py-0">
   <div class="container">
+    <div class="row g-3">
+      <div class="text-center mx-auto mb-1 wow fadeInUp" data-wow-delay="0.1s">
+        <h6 class="bg-dark text-center text-danger px-2 py-1">Baca review film terlengkap di Jebred</h6>
+      </div>
+    </div>
 
     <div class="owl-carousel project-carousel wow fadeInUp" data-wow-delay="0.1s">
       <?php foreach ($review as $article) : ?>
